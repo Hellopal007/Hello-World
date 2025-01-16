3b63b1e38c998265eeed2fc196efc492db6cff16
QuackersNCheese <Quacktastrophe@protonmail.com>
2025-01-15 17:40:27 -0700

    made responsive to screen size

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 function toggleDark() {
     let bodyElement = document.body;
     bodyElement.classList.toggle("dark-mode");
+    
     let modeButton = document.getElementById("dark");
     if(modeButton.innerHTML === "Dark Mode") {
         modeButton.innerHTML = "Light Mode";
